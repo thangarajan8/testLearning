faa54e1f83f3dcd763c87920fbfe36bafcd3bc6b
Thangarajan.P <thangam.rajan8@gmail.com>
2020-08-02 17:45:42 +0530

    lambda

---
+++ s7.py
@@ -0,0 +1,18 @@
+import json
+import boto3
+import urllib3
+
+def lambda_handler(event, context):
+    
+    # TODO implement
+    url="http://snap.stanford.edu/data/amazon/productGraph/categoryFiles/reviews_Amazon_Instant_Video_5.json.gz"
+    bucket = 'thanga-delete' #your s3 bucket
+    key = 'testing/sales.zip' #your desired s3 path or filename
+
+    s3=boto3.client('s3')
+    http=urllib3.PoolManager()
+    s3.upload_fileobj(http.request('GET', url,preload_content=False), bucket, key)
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
